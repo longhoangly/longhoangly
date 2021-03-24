1aae31040d72771d1b7d557f35b377ce1c3b5f0a
Long Ly <longhoangly@gmail.com>
2021-03-24 21:21:21 +0700

    add button for changing night mode - in progress

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+
+$(document).ready(function () {
+    $("#nightMode").on('click', function () {
+        var isNight = $(this).attr('aria-pressed')
+
+        setNavBarNightMode(isNight)
+        setBodyNightMode(isNight)
+        // setFooterNightMode(isNight)
+    })
+});
+
+function setNavBarNightMode(isNight) {
+    if (isNight === "false") {
+        $("#navbar").removeClass("bg-light")
+        $("#navbar").removeClass("navbar-light")
+        $("#navbar.div.button").removeClass("btn-outline-light")
+
+        $("#navbar").addClass("bg-dark")
+        $("#navbar").addClass("navbar-dark")
+        $("#navbar.div.button").addClass("btn-outline-dark")
+    } else {
+        $("#navbar").removeClass("bg-dark")
+        $("#navbar").removeClass("navbar-dark")
+        $("#navbar.div.button").removeClass("btn-outline-dark")
+
+        $("#navbar").addClass("bg-light")
+        $("#navbar").addClass("navbar-light")
+        $("#navbar.div.button").addClass("btn-outline-light")
+    }
+}
+
+function setBodyNightMode(isNight) {
+
+    if (isNight === "false") {
+        $("#body").removeClass("bg-light")
+        $("#body").removeClass("text-dark")
+
+        $("#body").addClass("bg-dark")
+        $("#body").addClass("text-light")
+    } else {
+        $("#body").removeClass("bg-dark")
+        $("#body").removeClass("text-light")
+
+        $("#body").addClass("bg-light")
+        $("#body").addClass("text-dark")
+    }
+}
+
+function setFooterNightMode(isNight) {
+    if (isNight === "false") {
+        $("#footer").removeClass("bg-light")
+        $("#footer").removeClass("text-dark")
+        $("#footer_section.a").removeClass("text-dark")
+
+        $("#footer").addClass("bg-dark")
+        $("#footer").addClass("text-light")
+        $("#footer_section.a").attr("data-mdb-ripple-color", "light")
+    } else {
+        $("#footer").removeClass("bg-dark")
+        $("#footer").removeClass("text-light")
+        $("#footer_section.a").removeClass("text-light")
+
+        $("#footer").addClass("bg-light")
+        $("#footer").addClass("text-dark")
+        $("#footer_section.a").attr("data-mdb-ripple-color", "dark")
+    }
+}
