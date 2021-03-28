982ab6c87d88b7a213dbc11d8886333bfc87cd6f
Long Ly <longhoangly@gmail.com>
2021-03-28 16:36:38 +0700

    change font body complete home page content change night mode icon change jquery to use toggleClass method instead add and remove

---
+++ Notes.md
@@ -0,0 +1,7 @@
+Custom fonts
+
+<link href="https://fonts.googleapis.com/css2?family=K2D:wght@300&display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css2?family=Livvic:wght@300&display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css2?family=Gotu&display=swap" rel="stylesheet">
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,15 @@
 
 $(document).ready(function () {
 
+    $("#download").on("click", function () {
+        window.open('https://github.com/longhoangly/longhoangly.github.io', '_blank')
+    })
+
     $("#nightMode").on("click", function () {
-
-        // Everytime users click on night mode button, it means they want to change mode, then reversed current night mode value is user's expectation!
+        // Everytime users click on night mode button, it means they want to change mode
+        // Then reversed current night mode value is user's expectation!
         let isNight = (!$("body").hasClass("bg-dark")).toString()
-        console.log("save localStorage isNight", isNight)
         localStorage.setItem("isNight", isNight)
-
         setNavBarNightMode(isNight)
         setBodyNightMode(isNight)
         setFooterNightMode(isNight)
@@ -15,53 +17,63 @@ $(document).ready(function () {
 
     // Set night mode for website based on local storage memory
     let isNight = localStorage.getItem("isNight")
-    console.log("get localStorage isNight", isNight)
-
     setNavBarNightMode(isNight)
     setBodyNightMode(isNight)
     setFooterNightMode(isNight)
-    $("body").attr("style", "visibility: visible !important");
+    $("body").attr("style", "visibility: visible !important")
+
+
+    // Change navigation menu icon
+    $("#changeToggle").on("click", function () {
+
+        var isCollapsed = $("#navbarSupportedContent").hasClass("show")
+        $("#changeToggle>i").toggleClass("fa-times", !isCollapsed)
+        $("#changeToggle>i").toggleClass("fa-bars", isCollapsed)
+    })
 })
 
 $(window).on("load", function () {
 
     // Set sticky responsive footer
-    var offsetHeight = $("footer").outerHeight() + $("nav").outerHeight()
-    var minHeight = `calc(100vh - ${offsetHeight}px)`
+    var footerHeight = $("footer").outerHeight()
+    console.log("footer height", footerHeight)
 
-    $("#main").attr("style", `min-height: ${minHeight};`);
-    console.log("main min-height", minHeight)
+    var headerHeight = $("nav").outerHeight()
+    console.log("header height", headerHeight)
+
+    var minHeight = `calc(100vh - ${footerHeight + headerHeight}px)`
+    console.log("main min height", minHeight)
+
+    $("#main").attr("style", `min-height: ${minHeight};`)
 })
-
-function setBodyNightMode(isNight) {
-
-    if (isNight == "true") {
-        $("body").addClass("bg-dark text-light").removeClass("bg-light text-dark")
-    } else {
-        $("body").addClass("bg-light text-dark").removeClass("bg-dark text-light")
-    }
-}
 
 function setNavBarNightMode(isNight) {
 
-    if (isNight == "true") {
-        $("nav").addClass("bg-dark navbar-dark").removeClass("bg-light navbar-light")
-        $("nav>div>button").addClass("btn-outline-light").removeClass("btn-outline-dark")
-        $("#nightMode").text("Light Mode")
-    } else {
-        $("nav").addClass("bg-light navbar-light").removeClass("bg-dark navbar-dark")
-        $("nav>div>button").addClass("btn-outline-dark").removeClass("btn-outline-light")
-        $("#nightMode").text("Night Mode")
-    }
+    $("nav").toggleClass("bg-dark navbar-dark", isNight == "true")
+    $("nav>div>button").toggleClass("btn-outline-light", isNight == "true")
+
+    $("nav").toggleClass("bg-light navbar-light", isNight == "false")
+    $("nav>div>button").toggleClass("btn-outline-dark", isNight == "false")
+
+    $("#nightMode>i").toggleClass("fa-moon", isNight == "false")
+    $("#nightMode>i").toggleClass("fa-sun", isNight == "true")
+
+    $("#nightMode").attr("title", isNight == "true" ? "Switch to the light theme" : "Switch to the dark theme")
+}
+
+function setBodyNightMode(isNight) {
+
+    $("body").toggleClass("bg-dark text-light", isNight == "true")
+    $("body").toggleClass("bg-light text-dark", isNight == "false")
 }
 
 function setFooterNightMode(isNight) {
 
-    if (isNight == "true") {
-        $("footer").addClass("bg-dark text-light").removeClass("bg-light text-dark")
-        $("footer section>a").addClass("text-light").removeClass("text-dark").attr("data-mdb-ripple-color", "light")
-    } else {
-        $("footer").addClass("bg-light text-dark").removeClass("bg-dark text-light")
-        $("footer section>a").addClass("text-dark").removeClass("text-light").attr("data-mdb-ripple-color", "dark")
-    }
+    $("footer").toggleClass("bg-dark text-light", isNight == "true")
+    $("footer section>a").toggleClass("text-light", isNight == "true")
+
+    $("footer").toggleClass("bg-light text-dark", isNight == "false")
+    $("footer section>a").toggleClass("text-dark", isNight == "false")
+
+    $("footer section>a").attr("data-mdb-ripple-color", isNight ? "light" : "dark")
 }
