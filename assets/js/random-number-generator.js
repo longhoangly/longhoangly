424619443626359ac75d60fdebbe49dcbdd0cca2
Long Ly <longhoangly@gmail.com>
2021-04-06 13:07:15 +0700

    Update UI, add more elements / options

---
+++ assets/js/random-number-generator.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $("#nightMode").on("click", function () {
+        let isNight = localStorage.getItem("isNight") || false.toString()
+        setButtonNightMode(isNight)
+    })
+
+    let isNight = localStorage.getItem("isNight") || false.toString()
+    setButtonNightMode(isNight)
+})
+
+function setButtonNightMode(isNight) {
+
+    $("button").toggleClass("btn-outline-light", isNight == "true")
+    $("button").toggleClass("btn-outline-dark", isNight == "false")
+}
