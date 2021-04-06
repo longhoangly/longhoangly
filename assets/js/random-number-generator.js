e6f4cc8dbab4bc61626446ff4b7b968414210ac6
Long Ly <longhoangly@gmail.com>
2021-04-06 18:04:00 +0700

    inprogress implement code behind

--- assets/js/random-number-generator.js
+++ assets/js/random-number-generator.js
@@ -7,10 +7,26 @@ $(document).ready(function () {
 
     let isNight = localStorage.getItem("isNight") || false.toString()
     setButtonNightMode(isNight)
+
+    $("#generate").on("click", function () {
+        var radioValue = $("input[name='separator']:checked").val();
+        $("#result").text(radioValue)
+        generateRandomNumbers(1, 1, 10, ",", true)
+    })
 })
 
 function setButtonNightMode(isNight) {
 
     $("button").toggleClass("btn-outline-light", isNight == "true")
     $("button").toggleClass("btn-outline-dark", isNight == "false")
+}
+
+function generateRandomNumbers(qty, from, to, separator, isUnique) {
+
+    var range = parseInt(from) - parseInt(to)
+    console.log("range", range)
+
+    var array = Array(range).fill().map((x,i)=>i);
+    console.log("array", array)
+
 }
